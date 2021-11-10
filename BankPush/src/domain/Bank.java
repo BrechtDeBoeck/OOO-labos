18ea6249fd6431f5aaeb025095da0e8b02faeb88
sander-arits <sander.arits@student.ucll.be>
2021-11-10 14:49:06 +0100

    BankPush update

--- BankPush/src/domain/Bank.java
+++ BankPush/src/domain/Bank.java
@@ -40,6 +40,6 @@ public class Bank extends Observable {
 
     public void rekeningAdded(){
         setChanged();
-        notifyObservers(getNewestRekening());
+        notifyObservers(getNewestRekening().getValue());
     }
 }
